517056dfdd878814a3488c6dfef01556c42cb072
KBU <rjsehd0390@kbu.ac.kr>
2026-09-11 09:57:25 +0900

    2번째 주

---
+++ Week02/Sample02-1/src/Main.java
@@ -0,0 +1,12 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+// soutp 치면 출력명령어 자동입력
+
+void main()
+{
+    System.out.println("경복대학교");
+    System.out.println("Computer");
+    System.out.println("qkrehdrjs");
+    System.out.println("");
+
+}
