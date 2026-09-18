97d6d6c8696d82451c449d0b0b2dac996edd7c98
leewoojoo123 <super2041@gmail.com>
2026-09-18 11:11:18 +0900

    3주차 수업내용

--- Week03/Sample03-5/src/Main.java
+++ Week03/Sample03-5/src/Main.java
@@ -6,6 +6,6 @@ void main() {
 
     System.out.printf("우리나라는 %s입니다.\n",kor);
     System.out.printf("%s의 수도 : %s\n",kor, capital);
-    System.out.printf("%s의 면적 : %,d\u33a0\n",kor, size);
-    System.out.printf("%s의 인구 : %,d명\n",capital,pop);
+    System.out.printf("%s의 면적 : %,d\u33a0 (남한)\n",kor, size);
+    System.out.printf("%s의 인구 : %,d명 (2010년 기준)\n",capital,pop);
 }
